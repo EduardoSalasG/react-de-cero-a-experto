8def3edd8d520376f18ffe42595f7c249c40da09
Eduardo Salas Gonzalez <89312228+EduardoSalasG@users.noreply.github.com>
2025-05-07 17:35:14 -0400

    Fin seccion 6

---
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react"
+import { AddCategory, GifGrid } from "./components"
+
+function GifExpertApp() {
+
+    const [categories, setCategories] = useState(['One Punch'])
+
+    const onAddCategory = (newCategory) => {
+
+        if (categories.includes(newCategory)) return;
+
+        setCategories([newCategory, ...categories])
+        // categories.push(newCategory);
+        // console.log(category)
+    }
+
+    const category = ''
+
+    return (
+        <>
+            <h1>GifExpertApp</h1>
+
+            <AddCategory
+                onNewCategory={(value) => onAddCategory(value)}
+            ></AddCategory>
+
+            {
+                categories.map((category) => (
+                    <GifGrid
+                        key={category}
+                        category={category} />
+                ))
+            }
+        </>
+
+
+
+    )
+}
+
+export default GifExpertApp
---
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=NnwpwfXGxXt0xqt3225yVs6xNkXP2v0S&q=${category}&limit=10`
+
+    const resp = await fetch(url);
+
+    const { data } = await resp.json()
+
+    const gifs = data.map(img => (
+        {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+
+        }
+    ))
+    return gifs;
+}
