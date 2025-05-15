dd2c134ce31714ac0f8c793665628df654451a29
Eduardo Salas Gonzalez <89312228+EduardoSalasG@users.noreply.github.com>
2025-05-15 17:59:35 -0400

    Fin sección 9

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -5,10 +5,17 @@ import './index.css'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { HooksApp } from './HooksApp.jsx'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { Layout } from './05-useLayoutEffect/Layout'
+// import { Memorize } from './06-memos/Memorize'
+// import { MemoHook } from './06-memos/MemoHook'
+// import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/Padre'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FormWithCustomHook />
-  </StrictMode>,
+    <Padre></Padre>
+  </StrictMode>
 )
